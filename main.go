64e19029e2fcf5264a7332bde05e9586519357ac
Adam Hoelscher <adam.hoelscher@gmail.com>
2023-06-24 03:11:44 -0700

    finish day01

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Adam-Hoelscher/advent-of-code-2022/day01"
+)
+
+func main() {
+	fmt.Println("Beginning the Advent of Code!")
+	fmt.Println(day01.Answer())
+	fmt.Println("Merry Christmas!")
+}
